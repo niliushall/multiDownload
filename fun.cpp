c87acf9a49521d4574bbdf542feef5158a5d7055
niliushall <1459775963@qq.com>
2018-02-24 19:43:31 +0800

    temp

--- fun.cpp
+++ fun.cpp
@@ -13,12 +13,15 @@ int setnonblocking( int fd ) {
     return old_option;
 }
 
-int addfd( int epoll_fd, int fd ) {
+int addfd( int epoll_fd, int fd, bool oneshot ) {
     epoll_event event;
     event.data.fd = fd;
     event.events = EPOLLIN | EPOLLET;
+    if ( oneshot ) {
+        event.events |= EPOLLONESHOT;
+    }
     epoll_ctl( epoll_fd, EPOLL_CTL_ADD, fd, &event );
-    setnonblocking(fd);
+    setnonblocking( fd );
 }
 
 myDownload::myDownload() {
@@ -43,4 +46,35 @@ void * myDownload::download( void * arg ) {
         write( file_fd, tempfile.info.buf, sizeof( tempfile.info.buf ) );
         close( file_fd );
     }
+}
+
+void * myDownload::send_file( void *arg ) {
+    
+}
+
+void * myDownload::test( void * arg ) {
+    myDownload *temp = (myDownload *)arg;  //调用函数传入的对象
+    int sock_fd = temp->sock_fd;
+    int n = 0, ret = 0;
+    char tempname[10];
+    int r = recv( sock_fd, &n, sizeof( n ), 0 );  //当前为文件第几部分
+printf("recv_n = %d, r = %d\n", n, r);
+    sprintf( tempname, "temp_%d", n );
+    int file_fd = open( tempname, O_WRONLY | O_CREAT | O_TRUNC, 0774 );
+
+    while( true ) {
+        char buf[BUFFER_SIZE] = {0};
+        ret = recv( sock_fd, buf, BUFFER_SIZE-1, 0 );
+printf("ret = %d\n", ret);
+        if ( ret < 0 ) {
+            sleep(1);
+            continue;
+        } else if ( !ret ) {
+            break;
+        }
+        printf("buf:\n%s\n", buf);
+        write( file_fd, buf, BUFFER_SIZE );
+    }
+
+    close( file_fd );
 }
